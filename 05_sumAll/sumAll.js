534f215addce35c4261c00d60ce28b45d3fbb19e
Parker <parker.spann@icloud.com>
2024-07-05 23:14:48 -0400

    First 7 exercises complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) return "ERROR"
+    let result = 0
 
+
+    for (let i = Math.min(num1, num2); i < Math.max(num1, num2) +1; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeFarenheit) {
+  let degreeCelsius = (degreeFarenheit -32) * 5 / 9;
+  return Math.round(degreeCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeCelsius) {
+  let degreeFarenheit = degreeCelsius * 9 / 5 +32;
+  return Math.round(degreeFarenheit * 10) / 10;
 };
 
 // Do not edit below this line
